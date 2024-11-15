61687528d096b7e6af04071cd066b014686d3dda
Jose-Prince <joseprince148@gmail.com>
2024-11-15 15:34:45 -0600

    Feat: Initial frontend and traslation of chat execution to backend

---
+++ backend.py
@@ -0,0 +1,122 @@
+from typing import Any, Dict
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_core.tools import Tool
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_experimental.tools import PythonREPLTool
+
+def call_output_from_agent(input: str):
+    print("Start...")
+
+    intructions = """
+        You are an agent designed to write and execute Python code to answer questions.
+        You have access to a python REPL, which you can use to execute python code.
+        You have qrcode package installed.
+        You have to give always a name, never an id.+
+        If you get an error, debug your code and try again.
+        Only use the output of your code to answer the question.
+        You might know the answer without running any code, but you should still run the code to get the answer.
+        If it does not seem like you can write code to answer the question, just return "I don't know" as teh answer. 
+        """
+
+    base_prompt= hub.pull("langchain-ai/react-agent-template")
+
+    tools = [PythonREPLTool()]
+    
+    prompt = base_prompt.partial(instructions=intructions)
+
+    agent = create_react_agent(
+            prompt=prompt,
+            llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+            tools=tools,
+    )
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    netflix_agent_executor : AgentExecutor = create_csv_agent(
+        llm= ChatOpenAI(temperature=0, model='gpt-4'),
+        path="netflix_titles.csv",
+        verbose=True,
+        allow_dangerous_code = True,
+    )
+    hbo_agent_executor : AgentExecutor = create_csv_agent(
+        llm= ChatOpenAI(temperature=0, model='gpt-4'),
+        path="data.csv",
+        verbose=True,
+        allow_dangerous_code = True,
+    )
+    disney_agent_executor : AgentExecutor = create_csv_agent(
+        llm= ChatOpenAI(temperature=0, model='gpt-4'),
+        path="disney_plus_titles.csv",
+        verbose=True,
+        allow_dangerous_code = True,
+    )
+    primev_agent_executor : AgentExecutor = create_csv_agent(
+        llm= ChatOpenAI(temperature=0, model='gpt-4'),
+        path="amazon_prime_titles.csv",
+        verbose=True,
+        allow_dangerous_code = True,
+    )
+
+    def python_agent_executor_wrapper(original_prompt: str) -> dict[str,Any]:
+        return agent_executor.invoke({"input": original_prompt})
+    
+    tools = [
+        Tool(
+            name="Python Agent",
+            func=python_agent_executor_wrapper,
+            description="""useful when you need to transform natural language to python and execute the python code,
+            returning the results of the code execution
+            DOES NOT ACCEPT CODE AS INPUT""",
+        ),
+        Tool(
+            name="Prime_Video Agent",
+            func=primev_agent_executor.invoke,
+            description="""useful when you need to answer questions related to Prime Video series and movies in amazon_prime_titles.csv file,
+            takes as an input the entire question and returns the answer after running pandas calculation
+            """,
+        ),
+        Tool(
+            name="Disney+ Agent",
+            func=disney_agent_executor.invoke,
+            description="""useful when you need to answer questions related to Disney+ series and movies in disney_plus_titles.csv file,
+            takes as an input the entire question and returns the answer after running pandas calculation
+            """,
+        ),
+        Tool(
+            name="Max Agent",
+            func=hbo_agent_executor.invoke,
+            description="""useful when you need to answer questions related to Max series and movies in data.csv file,
+            takes as an input the entire question and returns the answer after running pandas calculation
+            """,
+        ),
+        Tool(
+            name="Netflix Agent",
+            func=netflix_agent_executor.invoke,
+            description="""useful when you need to answer questions related to Netflix series and movies in netflix_titles.csv file,
+            takes as an input the entire question and returns the answer after running pandas calculation
+            """,
+        ),
+    ]
+
+    grand_agent=create_react_agent(
+        prompt=prompt,
+        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+        tools=tools,
+    )
+
+    grand_agent_executor = AgentExecutor(
+        agent=grand_agent,
+        tools=tools,
+        verbose=True,
+    )
+
+    answer = grand_agent_executor.invoke(
+            {
+                "input": input
+            }
+        )
+    
+    return answer
--- main.py
+++ main.py
@@ -6,122 +6,63 @@ from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
+import streamlit as st
+from backend import call_output_from_agent
 
 load_dotenv()
 
 def main():
-    print("Start...")
+    st.set_page_config(
+        page_title="Movie-Python Agent",
+        page_icon="",
+        layout="wide"
+    )
 
-    intructions = """
-        You are an agent designed to write and execute Python code to answer questions.
-        You have access to a python REPL, which you can use to execute python code.
-        You have qrcode package installed.
-        You have to give always a name, never an id.+
-        If you get an error, debug your code and try again.
-        Only use the output of your code to answer the question.
-        You might know the answer without running any code, but you should still run the code to get the answer.
-        If it does not seem like you can write code to answer the question, just return "I don't know" as teh answer. 
+    st.title("Interactive Movie-Python Agent")
+    st.markdown(
+        """
+        <style>
+        .stApp{background-color:black;}
+        .title{color=#ff4b4b;}
+        .button{background-color: #ff4b4b; color: white;border-radius: 5px;}
+        .input{border: 1px solid #ff4b4b; border-radius: 5px;}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    instrucciones = """
+        - Siempre usa la herramienta, incluso si sabes la respuesta.
+        - Debes usar código de Python para responder.
+        - Eres un agente que puede escribir código.
+        - Solo respondes la pregunta escribiendo código se sabes la respuesta.
+        - Si no sabes la respuesta escribe "No sé la respuesta".
         """
 
-    base_prompt= hub.pull("langchain-ai/react-agent-template")
+    st.markdown(instrucciones)
 
-    tools = [PythonREPLTool()]
-    
-    prompt = base_prompt.partial(instructions=intructions)
-
-    agent = create_react_agent(
-            prompt=prompt,
-            llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
-            tools=tools,
-    )
-
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
-    netflix_agent_executor : AgentExecutor = create_csv_agent(
-        llm= ChatOpenAI(temperature=0, model='gpt-4'),
-        path="netflix_titles.csv",
-        verbose=True,
-        allow_dangerous_code = True,
-    )
-    hbo_agent_executor : AgentExecutor = create_csv_agent(
-        llm= ChatOpenAI(temperature=0, model='gpt-4'),
-        path="data.csv",
-        verbose=True,
-        allow_dangerous_code = True,
-    )
-    disney_agent_executor : AgentExecutor = create_csv_agent(
-        llm= ChatOpenAI(temperature=0, model='gpt-4'),
-        path="disney_plus_titles.csv",
-        verbose=True,
-        allow_dangerous_code = True,
-    )
-    primev_agent_executor : AgentExecutor = create_csv_agent(
-        llm= ChatOpenAI(temperature=0, model='gpt-4'),
-        path="amazon_prime_titles.csv",
-        verbose=True,
-        allow_dangerous_code = True,
-    )
-
-    def python_agent_executor_wrapper(original_prompt: str) -> dict[str,Any]:
-        return agent_executor.invoke({"input": original_prompt})
-    
-    tools = [
-        Tool(
-            name="Python Agent",
-            func=python_agent_executor_wrapper,
-            description="""useful when you need to transform natural language to python and execute the python code,
-            returning the results of the code execution
-            DOES NOT ACCEPT CODE AS INPUT""",
-        ),
-        Tool(
-            name="Prime_Video Agent",
-            func=primev_agent_executor.invoke,
-            description="""useful when you need to answer questions related to Prime Video series and movies in amazon_prime_titles.csv file,
-            takes as an input the entire question and returns the answer after running pandas calculation
-            """,
-        ),
-        Tool(
-            name="Disney+ Agent",
-            func=disney_agent_executor.invoke,
-            description="""useful when you need to answer questions related to Disney+ series and movies in disney_plus_titles.csv file,
-            takes as an input the entire question and returns the answer after running pandas calculation
-            """,
-        ),
-        Tool(
-            name="Max Agent",
-            func=hbo_agent_executor.invoke,
-            description="""useful when you need to answer questions related to Max series and movies in data.csv file,
-            takes as an input the entire question and returns the answer after running pandas calculation
-            """,
-        ),
-        Tool(
-            name="Netflix Agent",
-            func=netflix_agent_executor.invoke,
-            description="""useful when you need to answer questions related to Netflix series and movies in netflix_titles.csv file,
-            takes as an input the entire question and returns the answer after running pandas calculation
-            """,
-        ),
+    st.markdown("### Ejemplos: ")
+    ejemplos = [
+        "Calcula la suma de 2 y 3", 
+        "Genera una lista del 1 al 10", 
+        "Crea una función que calcule el factorial de un número",
+        "Crea un juego básico de snake con la librería pygame"
     ]
 
-    grand_agent=create_react_agent(
-        prompt=prompt,
-        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
-        tools=tools,
-    )
+    example = st.selectbox("Selecciona un ejemplo:", ejemplos)
 
-    grand_agent_executor = AgentExecutor(
-        agent=grand_agent,
-        tools=tools,
-        verbose=True,
-    )
-
-    print(
-        grand_agent_executor.invoke(
-            {
-                "input": "Give me a function for obtainig factorial for a number"
-            }
-        )
-    )
+    if st.button("Ejecutar ejemplo"):
+        user_input = example
+        print(user_input)
+        try:
+            respuesta = call_output_from_agent(user_input)
+            st.markdown("### Respuesta del agente: ")
+            if "output" in respuesta:
+                st.code(respuesta["output"], language="python")
+            else:
+                st.error("No se obtuvo una clave de salida válida.")
+        except Exception as e:
+            st.error(f"Error en el agente: {str(e)}")
 
 if __name__ == '__main__':
     main()
